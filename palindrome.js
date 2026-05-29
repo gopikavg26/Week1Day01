a37e0018887f153b858705b0e9d80d342289b4b7
gopikavg26 <gopika.vg26@gmail.com>
2026-05-29 09:53:24 +0530

    Add files via upload

---
+++ palindrome.js
@@ -0,0 +1,3 @@
+//4.Create a JavaScript function that accepts a string, reverses it, and checks if the reversed 
+// string is a palindrome, then test your function with various strings and print the results. 
+

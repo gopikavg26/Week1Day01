aaf79bee27ba61365ba3d8f404b4a90d94b7ad93
gopikavg26 <gopika.vg26@gmail.com>
2026-05-29 14:48:02 +0530

    Add files via upload

--- palindrome.js
+++ palindrome.js
@@ -1,3 +1,35 @@
 //4.Create a JavaScript function that accepts a string, reverses it, and checks if the reversed 
 // string is a palindrome, then test your function with various strings and print the results. 
 
+// let name='Gopika';
+// console.log(name.split('').reverse());
+
+
+// function reverseName(){
+//     let object='UMBRELLA';
+//     console.log(object.split('').reverse().join(''));
+// }
+// reverseName();
+
+// function reverseName(name){
+//     let reversed= name.split('').reverse().join('');
+//     console .log(reversed);
+// }
+// reverseName('Tree');
+
+
+function palindrome(name){
+    let reversed="";
+    for(let i= name.length-1;i>=0;i--){
+        reversed = reversed+name[i];
+    }
+
+if(name===reversed){
+    console.log('The word is palindrome');
+}
+else{
+    console.log('The word is not palindrome');
+}
+}
+palindrome('level');
+palindrome('tree');
